89e9068547d19405a7c1a98c095a9f5a614894c6
Dean Hamstead <dean@fragfest.com.au>
2026-02-23 23:16:00 +1100

    maint/ Don't assume awk isn't there.

--- maint/scripts/01_install_oracle_xe.bash
+++ maint/scripts/01_install_oracle_xe.bash
@@ -18,7 +18,7 @@ sha256sum oracle-xe_11.2.0-1.0_amd64.deb
 dpkg --install oracle-xe_11.2.0-1.0_amd64.deb
 
 # Hack needed because oracle configuration looks for awk in /bin instead of $PATH
-ln -s /usr/bin/awk /bin/awk
+[ -f '/bin/awk' ] || ln -s /usr/bin/awk /bin/awk
 # Oracle also needs this:
 mkdir -p /var/lock/subsys
 
